364f0d9a56c98f1222f0846f6e5cc0f340c8b424
Ksenia Ivanova <fen10wa@yandex.ru>
2023-06-17 22:41:21 +0300

    Обновила пути в Api

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
   const requestHeaders = req.headers["access-control-request-headers"];
   if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
   }
   if (method === "OPTIONS") {
