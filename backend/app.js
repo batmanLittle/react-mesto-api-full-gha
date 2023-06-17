04416e4354acaa192365e63e619f8d5ee1830553
Ksenia Ivanova <fen10wa@yandex.ru>
2023-06-17 17:04:36 +0300

    Добавила обработку CORS запросов

--- backend/app.js
+++ backend/app.js
@@ -35,12 +35,34 @@ app.use(errorLogger);
 
 app.use(errors());
 
+const allowedCors = [
+  "http://batman.nomoredomains.rocks",
+  "https://api.batman.nomoredomains.rocks",
+  "localhost:3000",
+];
+
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
   res.status(statusCode).send({
     message: statusCode === 500 ? "На сервере произошла ошибка" : message,
   });
+
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers["access-control-request-headers"];
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", "true");
+  }
+  if (method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+    res.header("Access-Control-Allow-Headers", requestHeaders);
+
+    return res.status(200).send();
+  }
+
   next();
 });
 
