3fb08eabbadf148c6e749df38236f86b1a463fff
Ksenia Ivanova <fen10wa@yandex.ru>
2023-06-17 15:57:06 +0300

    Исправила путь к файлу

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const Auth = require("../utils/ Auth");
+const Auth = require("../utils/Auth");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
