37ef6d321ff1e599851e0e2493e1d0f4c2854b8f
Ksenia Ivanova <fen10wa@yandex.ru>
2023-06-17 20:14:11 +0300

    Обновила СORS

--- backend/app.js
+++ backend/app.js
@@ -36,8 +36,8 @@ app.use(errorLogger);
 app.use(errors());
 
 const allowedCors = [
-  "http://api.batman.nomoredomains.rocks",
-  "https://api.batman.nomoredomains.rocks",
+  "http://batman.nomoredomains.rocks",
+  "https://batman.nomoredomains.rocks",
   "localhost:3000",
 ];
 
@@ -53,14 +53,14 @@ app.use((err, req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
   const requestHeaders = req.headers["access-control-request-headers"];
   if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Origin", "origin");
     res.header("Access-Control-Allow-Credentials", "true");
   }
   if (method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
     res.header("Access-Control-Allow-Headers", requestHeaders);
 
-    return res.status(200).send();
+    return res.end();
   }
 
   next();
